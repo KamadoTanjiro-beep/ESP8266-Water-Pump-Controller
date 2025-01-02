55a3268bdceee92a14c0ce319b15d2937eb27137
APatra <43411221+desiFish@users.noreply.github.com>
2025-01-02 09:48:32 +0530

    Update README.md with detailed hardware connections and safety notes; revise feature list for clarity

--- README.md
+++ README.md
@@ -1,32 +1,76 @@
-
 # ESP8266 based AC 220V 50Hz Water Pump Controller
 
-## ALERT: This is a high-voltage high-ampere operation based project which deals with AC 220V and Amperes upto 5A-20A.
+## ALERT: This project involves high-voltage operations dealing with AC 220V-240V and currents up to 5A-20A.
 
 ## Hardware
 1. D1 Mini V2 NodeMcu 4M Bytes
-2. SSR-40DA Solid State Relay Module 3-32VDC/24-380VAC 40A (Choose a relay with 10 times more current rating than your pumps nomral current rating)
+2. (Costly)SSR-100DA Solid State Relay Module 3-32VDC/24-380VAC 100A (Choose a relay with 10 times higher current rating than your pump's normal current rating) or (Cheaper) you can use 30A Relay Modules in NC (Normally Closed Mode) - see note below.
 3. Piezo Electronic Buzzer Beep Alarm Bell Continuous Sound 3v-24v Buzzer Electromagnetic
-4. 2 Colour LED
+4. WS2812B RGB LED
 5. One Push Button
 6. SAIER SEN-PPI35 Float Level Switch
 7. DC Jacks, wires etc.
 
+## UPCOMING FEATURES
+- Check [Issues](https://github.com/KamadoTanjiro-beep/Water-Pump-Controller/issues) 
+
+## PICTURE(s)
+Coming Soon
+![Controller]()
+
+## CONNECTIONS
+I can help describe the schematic connections based on the code. Here's a text-based representation of how the components should be connected:
+
+### ESP8266 (D1 Mini) Connections:
+
+1. **SSR (Connected to D4/GPIO4)** 
+   - Signal pin to GPIO4 (D4)
+   - GND to GND
+   **if using Mechanical Relay (Connected to D4/GPIO4)**
+   - Signal pin to GPIO4 (D4)
+   - VCC to 5V
+   - GND to GND
+
+2. **Buzzer with BC547 (Connected to D5/GPIO5)**
+   - BC547 Base -> 1K resistor -> GPIO5 (D5)
+   - BC547 Collector -> Buzzer negative
+   - BC547 Emitter -> GND
+   - Buzzer positive -> 5V
+
+3. **Push Button (Connected to D12/GPIO12)**
+   - One terminal to GPIO12 (D12)
+   - Other terminal to 3.3V
+   - 10K pull-down resistor between GPIO12 and GND
+
+4. **Float Sensor (Connected to A0)**
+   - Signal wire to A0
+   - VCC to 3.3V
+   - GND to GND
+
+5. **WS2812B RGB LED (Connected to D14/GPIO14)**
+   - DIN (Data In) to GPIO14 (D14)
+   - VCC to 3.3V
+   - GND to GND
+
+Important Safety Notes:
+1. The relay's high voltage side should be handled by a qualified electrician
+2. Use appropriate isolation between high voltage and low voltage circuits
+3. Include proper fusing and protection
+4. Ensure proper heatsinking for the SSR
+5. Use appropriate gauge wires for the current ratings
+
+## FEATURES V1.0.2
+1. Removed music as ending tone
+2. Added BC547 transistor to drive the buzzer directly via 5V power for louder alerts
+3. Pressing the button during boot activates WiFi for OTA purposes
+4. Pressing the button during operation toggles the pump ON or OFF
+5. Other minor changes
 
 ## FEATURES V1.0.1
 1. Added music as ending tone
 
-   
-## UPCOMING FEATURES
-- Check [Issues](https://github.com/KamadoTanjiro-beep/Water-Pump-Controller/issues) 
-
-
-## Picture(s)
-Coming Soon
-![Controller]()
-
+* When using NC (Normally Closed) mode, the pump controller will use the relay to cut off power from the pump by switching from NC to NO (Normally Open). During pump startup, it draws up to 10 times more current which may cause arcs when the relay closes. In theory, if the pump is connected to NC and power is supplied, there won't be any arcs since the relay is already closed. We will use the relay only to disconnect the pump by switching it to NO mode and then turning the Main Switch OFF (It is important to use a main switch to turn the whole system off once your purpose is complete).
 
 ## License
-
-Distribute it freely but link back to this project or put some good words or attributes or donate (paypal link in profile) haha. You are own your own, I take no responsibility, if this thing explodes or does any damage on anything.
+Feel free to distribute this project but please link back to this repository. You can show your support by adding attributions, good words, or donating (PayPal link in profile). Please note that you use this at your own risk - I take no responsibility if this device causes any damage or malfunction. This project is not meant for 24/7 operation. Please turn off the system when your work is done - by turning off the system, I mean cutting off all power when not in use.
 
--- WaterPumpController.ino
+++ WaterPumpController.ino
@@ -1,7 +1,5 @@
 /*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp8266-client-server-wi-fi/
-
+  Aniket Patra
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files.
 
@@ -13,6 +11,8 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
+#include <Adafruit_NeoPixel.h>
+
 #include "global.h" //remove this
 
 #ifndef STASSID
@@ -23,606 +23,199 @@
 const char *ssid = pssid;     // replace "pssid" and with "STASSID"
 const char *password = ppass; // replace "ppass" and with "STAPSK"
 
-bool pumpFlag = 0;
-
-unsigned long previousMillis = 0;
-const long interval = 1000;
+bool pumpFlag = 1;
 
 const String newHostname = "PumpControl";
 
-#define red 13
-#define green 14
 #define pump 4
 #define buzzer 5
 #define button 12
 #define FLOAT_SENSOR_READ A0
 
-#define NOTE_B0 31
-#define NOTE_C1 33
-#define NOTE_CS1 35
-#define NOTE_D1 37
-#define NOTE_DS1 39
-#define NOTE_E1 41
-#define NOTE_F1 44
-#define NOTE_FS1 46
-#define NOTE_G1 49
-#define NOTE_GS1 52
-#define NOTE_A1 55
-#define NOTE_AS1 58
-#define NOTE_B1 62
-#define NOTE_C2 65
-#define NOTE_CS2 69
-#define NOTE_D2 73
-#define NOTE_DS2 78
-#define NOTE_E2 82
-#define NOTE_F2 87
-#define NOTE_FS2 93
-#define NOTE_G2 98
-#define NOTE_GS2 104
-#define NOTE_A2 110
-#define NOTE_AS2 117
-#define NOTE_B2 123
-#define NOTE_C3 131
-#define NOTE_CS3 139
-#define NOTE_D3 147
-#define NOTE_DS3 156
-#define NOTE_E3 165
-#define NOTE_F3 175
-#define NOTE_FS3 185
-#define NOTE_G3 196
-#define NOTE_GS3 208
-#define NOTE_A3 220
-#define NOTE_AS3 233
-#define NOTE_B3 247
-#define NOTE_C4 262
-#define NOTE_CS4 277
-#define NOTE_D4 294
-#define NOTE_DS4 311
-#define NOTE_E4 330
-#define NOTE_F4 349
-#define NOTE_FS4 370
-#define NOTE_G4 392
-#define NOTE_GS4 415
-#define NOTE_A4 440
-#define NOTE_AS4 466
-#define NOTE_B4 494
-#define NOTE_C5 523
-#define NOTE_CS5 554
-#define NOTE_D5 587
-#define NOTE_DS5 622
-#define NOTE_E5 659
-#define NOTE_F5 698
-#define NOTE_FS5 740
-#define NOTE_G5 784
-#define NOTE_GS5 831
-#define NOTE_A5 880
-#define NOTE_AS5 932
-#define NOTE_B5 988
-#define NOTE_C6 1047
-#define NOTE_CS6 1109
-#define NOTE_D6 1175
-#define NOTE_DS6 1245
-#define NOTE_E6 1319
-#define NOTE_F6 1397
-#define NOTE_FS6 1480
-#define NOTE_G6 1568
-#define NOTE_GS6 1661
-#define NOTE_A6 1760
-#define NOTE_AS6 1865
-#define NOTE_B6 1976
-#define NOTE_C7 2093
-#define NOTE_CS7 2217
-#define NOTE_D7 2349
-#define NOTE_DS7 2489
-#define NOTE_E7 2637
-#define NOTE_F7 2794
-#define NOTE_FS7 2960
-#define NOTE_G7 3136
-#define NOTE_GS7 3322
-#define NOTE_A7 3520
-#define NOTE_AS7 3729
-#define NOTE_B7 3951
-#define NOTE_C8 4186
-#define NOTE_CS8 4435
-#define NOTE_D8 4699
-#define NOTE_DS8 4978
-#define REST 0
+// Which pin on the esp8266 is connected to the NeoPixels?
+#define PIN 14
 
-// change this to make the song slower or faster
-int tempo = 90;
-
-// notes of the moledy followed by the duration.
-// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
-// !!negative numbers are used to represent dotted notes,
-// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
-int melody[] = {
-
-    // Game of Thrones
-    // Score available at https://musescore.com/user/8407786/scores/2156716
-
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16, // 1
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_E4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_E4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_E4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    8,
-    NOTE_C4,
-    8,
-    NOTE_E4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    -4,
-    NOTE_C4,
-    -4, // 5
-
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    4,
-    NOTE_C4,
-    4,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16, // 6
-    NOTE_D4,
-    -1, // 7 and 8
-    NOTE_F4,
-    -4,
-    NOTE_AS3,
-    -4,
-    NOTE_DS4,
-    16,
-    NOTE_D4,
-    16,
-    NOTE_F4,
-    4,
-    NOTE_AS3,
-    -4,
-    NOTE_DS4,
-    16,
-    NOTE_D4,
-    16,
-    NOTE_C4,
-    -1, // 11 and 12
-
-    // repeats from 5
-    NOTE_G4,
-    -4,
-    NOTE_C4,
-    -4, // 5
-
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    4,
-    NOTE_C4,
-    4,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16, // 6
-    NOTE_D4,
-    -1, // 7 and 8
-    NOTE_F4,
-    -4,
-    NOTE_AS3,
-    -4,
-    NOTE_DS4,
-    16,
-    NOTE_D4,
-    16,
-    NOTE_F4,
-    4,
-    NOTE_AS3,
-    -4,
-    NOTE_DS4,
-    16,
-    NOTE_D4,
-    16,
-    NOTE_C4,
-    -1, // 11 and 12
-    NOTE_G4,
-    -4,
-    NOTE_C4,
-    -4,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16,
-    NOTE_G4,
-    4,
-    NOTE_C4,
-    4,
-    NOTE_DS4,
-    16,
-    NOTE_F4,
-    16,
-
-    NOTE_D4,
-    -2, // 15
-    NOTE_F4,
-    -4,
-    NOTE_AS3,
-    -4,
-    NOTE_D4,
-    -8,
-    NOTE_DS4,
-    -8,
-    NOTE_D4,
-    -8,
-    NOTE_AS3,
-    -8,
-    NOTE_C4,
-    -1,
-    NOTE_C5,
-    -2,
-    NOTE_AS4,
-    -2,
-    NOTE_C4,
-    -2,
-    NOTE_G4,
-    -2,
-    NOTE_DS4,
-    -2,
-    NOTE_DS4,
-    -4,
-    NOTE_F4,
-    -4,
-    NOTE_G4,
-    -1,
-
-    NOTE_C5,
-    -2, // 28
-    NOTE_AS4,
-    -2,
-    NOTE_C4,
-    -2,
-    NOTE_G4,
-    -2,
-    NOTE_DS4,
-    -2,
-    NOTE_DS4,
-    -4,
-    NOTE_D4,
-    -4,
-    NOTE_C5,
-    8,
-    NOTE_G4,
-    8,
-    NOTE_GS4,
-    16,
-    NOTE_AS4,
-    16,
-    NOTE_C5,
-    8,
-    NOTE_G4,
-    8,
-    NOTE_GS4,
-    16,
-    NOTE_AS4,
-    16,
-    NOTE_C5,
-    8,
-    NOTE_G4,
-    8,
-    NOTE_GS4,
-    16,
-    NOTE_AS4,
-    16,
-    NOTE_C5,
-    8,
-    NOTE_G4,
-    8,
-    NOTE_GS4,
-    16,
-    NOTE_AS4,
-    16,
-
-    REST,
-    4,
-    NOTE_GS5,
-    16,
-    NOTE_AS5,
-    16,
-    NOTE_C6,
-    8,
-    NOTE_G5,
-    8,
-    NOTE_GS5,
-    16,
-    NOTE_AS5,
-    16,
-    NOTE_C6,
-    8,
-    NOTE_G5,
-    16,
-    NOTE_GS5,
-    16,
-    NOTE_AS5,
-    16,
-    NOTE_C6,
-    8,
-    NOTE_G5,
-    8,
-    NOTE_GS5,
-    16,
-    NOTE_AS5,
-    16,
-};
-
-// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
-// there are two values per note (pitch and duration), so for each note there are four bytes
-int notes = sizeof(melody) / sizeof(melody[0]) / 2;
-
-// this calculates the duration of a whole note in ms
-int wholenote = (60000 * 4) / tempo;
-
-int divider = 0, noteDuration = 0;
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS 1
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
+  pinMode(pump, OUTPUT);
+  digitalWrite(pump, LOW);
   Serial.begin(115200);
   Serial.println("Setup Started");
-
   pinMode(FLOAT_SENSOR_READ, INPUT);
-  pinMode(red, OUTPUT);
-  analogWrite(red, 5);
-  pinMode(green, OUTPUT);
-  pinMode(pump, OUTPUT);
+  pinMode(PIN, OUTPUT);
   pinMode(buzzer, OUTPUT);
   pinMode(button, INPUT);
-  // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  WiFi.hostname(newHostname.c_str()); // Set new hostname
-  WiFi.begin(ssid, password);
-
-  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.setPixelColor(0, pixels.Color(150, 0, 0));
+  pixels.show();
+  if (digitalRead(button) == 1)
   {
+    pixels.setPixelColor(0, pixels.Color(150, 150, 150));
+    pixels.show();
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.hostname(newHostname.c_str()); // Set new hostname
+    WiFi.begin(ssid, password);
 
-    Serial.println("Connection Failed");
-    buzzerFunc(500, 5);
-    break;
+    while (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+      Serial.println("Connection Failed");
+      lightFunc(500, 2);
+      break;
+    }
   }
-  // OTA UPDATE SETTINGS  !! CAUTION !! PROCEED WITH PRECAUTION
-  ArduinoOTA.onStart([]()
-                     {
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    // OTA UPDATE SETTINGS  !! CAUTION !! PROCEED WITH PRECAUTION
+    ArduinoOTA.setHostname("PumpControllerV1");
+
+    // No authentication by default
+    ArduinoOTA.setPassword("pumper");
+
+    ArduinoOTA.onStart([]()
+                       {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH) {
       type = "sketch";
-      buzzerFunc(300, 1);
+      lightFunc(300, 1);
     } else {  // U_FS
       type = "filesystem";
     }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-
-    //display.println(F("OTA Update\n"));
-
     delay(1000); });
-  ArduinoOTA.onEnd([]()
-                   {
-    //display.println(F("Rebooting"));
-    buzzerFunc(300, 1); });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
-                        {
-    //display.print("Progress: ");
-    buzzerFunc(1, 1); });
+    ArduinoOTA.onEnd([]()
+                     { lightFunc(300, 1); });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total)
+                          { lightFunc(1, 1); });
 
-  ArduinoOTA.onError([](ota_error_t error)
-                     {
-    buzzerFunc(1000, 2);
+    ArduinoOTA.onError([](ota_error_t error)
+                       {
+    lightFunc(1000, 2);
 
     //Serial.printf("Error[%u]: ", error);
     if (error == OTA_AUTH_ERROR) {
 
-      buzzerFunc(1000, 2);
+      lightFunc(1000, 3);
 
     } else if (error == OTA_BEGIN_ERROR) {
-      buzzerFunc(1000, 2);
+      lightFunc(1000, 4);
 
     } else if (error == OTA_CONNECT_ERROR) {
 
-      buzzerFunc(1000, 2);
+      lightFunc(1000, 5);
 
     } else if (error == OTA_RECEIVE_ERROR) {
 
-      buzzerFunc(1000, 2);
+      lightFunc(1000, 6);
 
     } else if (error == OTA_END_ERROR) {
-      buzzerFunc(1000, 2);
+      lightFunc(1000, 7);
     } });
-  ArduinoOTA.begin();
+    ArduinoOTA.begin();
+  }
   // OTA UPDATE END
-  analogWrite(red, 0);
+  pixels.clear();
+  pixels.show();
 }
 
 void loop()
 {
-  ArduinoOTA.handle();
-  unsigned long currentMillis = millis();
-  int signal = analogRead(FLOAT_SENSOR_READ);
-
-  if (currentMillis - previousMillis >= interval)
+  if (WiFi.status() == WL_CONNECTED)
   {
-    Serial.println(signal);
-    previousMillis = currentMillis;
+    ArduinoOTA.handle();
   }
 
-  if (signal > 900)
-  { // NOT FULL
-    if (digitalRead(button) == 1)
+  int signal = analogRead(FLOAT_SENSOR_READ);
+
+  if (digitalRead(button) == 1)
+  {
+    digitalWrite(buzzer, HIGH);
+    delay(300);
+    digitalWrite(buzzer, LOW);
+    delay(50);
+
+    digitalWrite(buzzer, HIGH);
+    delay(300);
+    digitalWrite(buzzer, LOW);
+    delay(50);
+
+    Serial.println("PUMP ON");
+    if (pumpFlag == 0)
     {
-      digitalWrite(buzzer, HIGH);
-      delay(100);
-      digitalWrite(buzzer, LOW);
-      delay(50);
-
-      digitalWrite(buzzer, HIGH);
-      delay(300);
-      digitalWrite(buzzer, LOW);
-      delay(50);
-
-      digitalWrite(buzzer, HIGH);
-      delay(300);
-      digitalWrite(buzzer, LOW);
-      delay(50);
-
-      digitalWrite(buzzer, HIGH);
-      delay(500);
-      digitalWrite(buzzer, LOW);
-      delay(100);
-
-      Serial.println("PUMP ON");
-      digitalWrite(pump, HIGH);
+      digitalWrite(pump, LOW);
       pumpFlag = 1;
     }
+    else if (pumpFlag == 1)
+    {
+      digitalWrite(pump, HIGH);
+      pumpFlag = 0;
+    }
+  }
+
+  if (signal < 100)
+  { // NOT FULL
     if (pumpFlag == 1)
     {
-      digitalWrite(green, HIGH);
-      delay(200);
-      digitalWrite(green, LOW);
-      delay(200);
+      pixels.setPixelColor(0, pixels.Color(0, 150, 0));
+      pixels.show();
+      delay(300);
+      pixels.clear();
+      pixels.show();
+      delay(300);
     }
     else
     {
-      analogWrite(red, 50);
-      delay(500);
-      analogWrite(red, 0);
-      delay(400);
+      pixels.setPixelColor(0, pixels.Color(150, 150, 0));
+      pixels.show();
+      delay(300);
+      pixels.clear();
+      pixels.show();
+      delay(300);
     }
   }
-  else if (signal < 100)
+  else if (signal > 900)
   { // WHEN TANK IS FULL
-
-    Serial.println("PUMP OFF");
-    digitalWrite(pump, LOW);
+    digitalWrite(pump, HIGH);
     pumpFlag = 0;
-    digitalWrite(red, 50);
-    buzzDone();
+    Serial.println("PUMP OFF");
 
     while (true)
     {
-      if (digitalRead(button) == 1)
-      {
-        digitalWrite(red, 50);
-        digitalWrite(buzzer, HIGH);
-        delay(1000);
-        digitalWrite(buzzer, LOW);
-        delay(500);
-        ESP.restart();
-      }
-      digitalWrite(red, 50);
-      delay(500);
+      pixels.setPixelColor(0, pixels.Color(0, 0, 150));
+      pixels.show();
+      buzzDone(1);
+      pixels.clear();
+      pixels.show();
+      delay(150);
     }
   }
 }
 
-void buzzerFunc(int delay1, byte num)
+void lightFunc(int delay1, byte num)
 {
   for (int i = 0; i < num; i++)
   {
-    digitalWrite(buzzer, HIGH);
+    pixels.setPixelColor(0, pixels.Color(150, 0, 0));
+    pixels.show();
     delay(delay1);
-    digitalWrite(buzzer, LOW);
+    pixels.clear();
+    pixels.show();
     delay(delay1);
   }
 }
 
-void buzzDone()
+void buzzDone(byte x)
 {
-  /* for (int i = 0; i < 7; i++) {
-    digitalWrite(buzzer, HIGH);
-    delay(1000);
-    digitalWrite(buzzer, LOW);
-    delay(500);
-  }*/
-
-  // iterate over the notes of the melody.
-  // Remember, the array is twice the number of notes (notes + durations)
-  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2)
+  for (byte i = 0; i < x; i++)
   {
-
-    // calculates the duration of each note
-    divider = melody[thisNote + 1];
-    if (divider > 0)
-    {
-      // regular note, just proceed
-      noteDuration = (wholenote) / divider;
-    }
-    else if (divider < 0)
-    {
-      // dotted notes are represented with negative durations!!
-      noteDuration = (wholenote) / abs(divider);
-      noteDuration *= 1.5; // increases the duration in half for dotted notes
-    }
-
-    // we only play the note for 90% of the duration, leaving 10% as a pause
-    tone(buzzer, melody[thisNote], noteDuration * 0.9);
-
-    // Wait for the specief duration before playing the next note.
-    delay(noteDuration);
-
-    // stop the waveform generation before the next note.
-    noTone(buzzer);
+    digitalWrite(buzzer, HIGH);
+    delay(500);
+    digitalWrite(buzzer, LOW);
+    delay(300);
   }
 }
